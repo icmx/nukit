7a21226801c24cd16886353097b0c4c8b0cb099f
icmx <32983315+icmx@users.noreply.github.com>
2022-02-27 00:28:26 +1000

    add summary for timestamp utility

--- src/timestamps/ts/index.ts
+++ src/timestamps/ts/index.ts
@@ -1,8 +1,25 @@
 export type TsOptions = {
+  /**
+   * Date to use for timestamp creation. Current by default.
+   */
   from?: Date;
+
+  /**
+   * Option to handle milliseconds in timestamp.
+   *
+   * *Note: `'ceil'`, `'floor'`, `'round'` and `'trunc'` are internally
+   * uses appropriate `Math` methods.
+   *
+   * Math reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+   * @default undefined
+   */
   ms?: keyof Pick<Math, 'ceil' | 'floor' | 'round' | 'trunc'>;
 };
 
+
+/**
+ * Returns a Unix timestamp for current or specified date.
+ */
 export const ts = (options: TsOptions = {}) => {
   const { from, ms }: TsOptions = {
     from: new Date(),
