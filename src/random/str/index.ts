a7d6ef4140a81c35916fc7e4af7b5e3e32c08937
icmx <32983315+icmx@users.noreply.github.com>
2022-03-04 12:49:55 +1000

    add str random utility

--- src/random/str/index.ts
+++ src/random/str/index.ts
@@ -1,4 +1,7 @@
+import { WORD_CHARS } from '../../constants';
 import { WithRNGOption } from '../../types/WithRNGOption';
+import { char } from '../char';
+import { int } from '../int';
 
 export type StrOptions = {
   /**
@@ -17,3 +20,30 @@ export type StrOptions = {
    */
   maxLength?: number;
 } & WithRNGOption;
+
+/**
+ * Returns random string value.
+ */
+export const str = (options: StrOptions = {}): string => {
+  const { alphabet, minLength, maxLength, rng }: StrOptions = {
+    alphabet: WORD_CHARS,
+    minLength: 0,
+    maxLength: 1,
+    rng: Math.random,
+    ...options,
+  };
+
+  const length = int({ min: minLength, max: maxLength, rng });
+
+  if (length < 0) {
+    throw new TypeError('Length should be more than or equal to 0');
+  }
+
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+    result += char({ alphabet, rng });
+  }
+
+  return result;
+};
