b3bd30b9782215b92abe83065a3ad136a168ab68
icmx <32983315+icmx@users.noreply.github.com>
2022-03-20 18:13:05 +1000

    add tests for throwon utility

---
+++ src/error/index.test.ts
@@ -0,0 +1,37 @@
+import { nameOf } from '../../test/utils/nameOf';
+import { throwOn } from './index';
+
+describe(nameOf(throwOn), () => {
+  const ofFalse = () => false;
+  const ofTrue = () => true;
+
+  it('should not throw an error if predicate returns false', () => {
+    expect(() => {
+      throwOn(false);
+    }).not.toThrow();
+
+    expect(() => {
+      // @ts-ignore
+      throwOn(0 === 1);
+    }).not.toThrow();
+
+    expect(() => {
+      // @ts-ignore
+      throwOn(ofFalse());
+    }).not.toThrow();
+  });
+
+  it('should throw an error if predicate returns true', () => {
+    expect(() => {
+      throwOn(true);
+    }).toThrow();
+
+    expect(() => {
+      throwOn(ofTrue());
+    }).toThrow();
+
+    expect(() => {
+      throwOn(0 === 0);
+    }).toThrow();
+  });
+});
