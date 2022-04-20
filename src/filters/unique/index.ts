b22ba6eebbcddbc8390b68bd5d5c257a806707ed
icmx <32983315+icmx@users.noreply.github.com>
2022-04-20 09:55:17 +0300

    add summaries and update readme

--- src/filters/unique/index.ts
+++ src/filters/unique/index.ts
@@ -1,3 +1,15 @@
+/**
+ * Returns an array without duplicates, a predicate version. Useful for
+ * array chaining.
+ *
+ * Please note that this predicate internally much slower than standard
+ * unique utility.
+ * @example
+ * array
+ *  .map(i => i + 1)
+ *  .filter(uniquePredicate) // <- here it is
+ *  .join(':')
+ */
 export const uniquePredicate = <T = unknown>(
   value: T,
   index: number,
@@ -6,6 +18,9 @@ export const uniquePredicate = <T = unknown>(
   return array.indexOf(value) === index;
 };
 
+/**
+ * Returns an array without duplicates.
+ */
 export const uniqie = <T = unknown>(value: T[]): T[] => {
   return Array.from(new Set<T>(value));
 };
